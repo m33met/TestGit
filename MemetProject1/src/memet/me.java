62fa8e8f29b5378e038d2be315a132c5686f64bd
m33me <m33me@DESKTOP-JM71L1R.PUBLIC.CO.FAIRFAX.VA.US>
2020-01-25 16:27:35 -0500

    First commit from Maryland.

--- MemetProject1/src/memet/me.java
+++ MemetProject1/src/memet/me.java
@@ -1,25 +1,11 @@
 package memet;
 
 public class me {
-
+	
+	
 	public static void main(String[] args) {
-
-		System.out.print("this is memet");
-		System.out.print("this is memet");
+		System.out.println("My name is memet. I am working from Maryland.");
 	}
-
-	public static void test11() {
-
-	}
-
-	public static void test1() {
-
-		System.out.print("this is memet");
-		System.out.print("this is memet");
-
-		System.out.print("this is memet");
-		System.out.print("this is memet");
-
-	}
-
+	
+	
 }
