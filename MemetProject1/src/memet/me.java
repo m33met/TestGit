6edd9902c2bb83ecbc72749181d7af51b9575b97
m33me <m33me@DESKTOP-JM71L1R.PUBLIC.CO.FAIRFAX.VA.US>
2020-01-25 16:02:11 -0500

    I fixed mamet1 code

--- MemetProject1/src/memet/me.java
+++ MemetProject1/src/memet/me.java
@@ -8,19 +8,21 @@ public class me {
 		System.out.print("this is memet");
 	}
 
-	public static void test() {
+	public static void test11() {
 		System.out.print("this is memet");
 		System.out.print("this is memet");
+	
+
+	System.out.print("this is memet");
+	System.out.print("this is memet");
+
 	}
 
-	System.out.print("this is memet");System.out.print("this is memet");
-
-
-
-	public static void test() {
+	public static void test21() {
 		System.out.print("this is memet");
 		System.out.print("this is memet");
-	}System.out.print("this is memet");
+	System.out.print("this is memet");
+	System.out.println("I am " );
 }
 
-}}
+}
