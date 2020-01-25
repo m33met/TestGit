2d2cdc1acd5aa26fe5f1c6eeb0e70f8c9ef3f4fc
m33me <m33me@DESKTOP-JM71L1R.PUBLIC.CO.FAIRFAX.VA.US>
2020-01-25 15:47:27 -0500

    I do what ever I eant.

--- MemetProject1/src/memet/me.java
+++ MemetProject1/src/memet/me.java
@@ -1,18 +1,32 @@
 package memet;
 
-import com.sun.tools.javac.launcher.Main;
-
 public class me {
-	
+
 	public static void main(String[] args) {
-		
+
+		System.out.print("this is memet");
+		System.out.print("this is memet");
+	}
+
+	public static void test() {
 		System.out.print("this is memet");
 		System.out.print("this is memet");
 	}
 	
-	public static void test() {
 	System.out.print("this is memet");
 	System.out.print("this is memet");
-	}
+}
+
+public static void test() {
+	System.out.print("this is memet");
+	System.out.print("this is memet");
+}		System.out.print("this is memet");
+System.out.print("this is memet");
+}
+
+public static void test() {
+System.out.print("this is memet");
+System.out.print("this is memet");
+}
 
 }
