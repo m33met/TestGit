b868cee2cb8d6461203dde50714bb7d15f223307
m33me <m33me@DESKTOP-JM71L1R.PUBLIC.CO.FAIRFAX.VA.US>
2020-01-25 17:09:37 -0500

    second commit

--- MemetProject1/src/memet/me.java
+++ MemetProject1/src/memet/me.java
@@ -5,6 +5,9 @@ public class me {
 	
 	public static void main(String[] args) {
 		System.out.println("My name is memet. I am working from Maryland.");
+
+		System.out.println("This is my second line of code.");
+
 	}
 	
 	
