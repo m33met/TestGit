bf4ed13fe39ecceed33632c8926685df9d7f7873
m33me <m33me@DESKTOP-JM71L1R.PUBLIC.CO.FAIRFAX.VA.US>
2020-01-25 15:48:58 -0500

    deleted some other code

--- MemetProject1/src/memet/me.java
+++ MemetProject1/src/memet/me.java
@@ -12,18 +12,15 @@ public class me {
 		System.out.print("this is memet");
 		System.out.print("this is memet");
 	}
-	
-	System.out.print("this is memet");
-	System.out.print("this is memet");
+
+	System.out.print("this is memet");System.out.print("this is memet");
+
+
+
+	public static void test() {
+		System.out.print("this is memet");
+		System.out.print("this is memet");
+	}System.out.print("this is memet");
 }
 
-public static void test() {
-	System.out.print("this is memet");
-	System.out.print("this is memet");
-}		System.out.print("this is memet");
-System.out.print("this is memet");
-}
-
-}
-
-}
+}}
