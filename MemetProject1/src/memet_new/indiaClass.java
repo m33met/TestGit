6538658c9c3d89628b1ea63f1580cfe7fcbda095
m33me <m33me@DESKTOP-JM71L1R.fios-router.home>
2020-02-01 13:53:48 -0500

    memet 105

--- MemetProject1/src/memet_new/indiaClass.java
+++ MemetProject1/src/memet_new/indiaClass.java
@@ -5,6 +5,8 @@ public class indiaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" h id id s vhshr ");
+		
+		System.out.println(" let me add a new class and pull this code to other one.");
 	}
 
 }
